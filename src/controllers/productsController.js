c3fb9e90f05e1a0d0bba2f6626d72b4d046446f6
Renard Bergson <renardrock@gmail.com>
2023-05-24 10:07:54 -0300

    habilitando o método get para pesquisar por ID ou listar todos os produtos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,7 +1,31 @@
 const ProductsModel = require('../models/productsModel')
 
 async function get (req, res) {
-    const products = await ProductsModel.find()
+    const {id} = req.params
+
+    const obj = id ? {_id: id} : null 
+    // is there an ID? So, obj is an objet with {_id: id}, else, obj is null. It might be "{}" as well.
+
+    /* 
+    OR...
+    
+    const obj = {}
+
+    if (id) {
+        obj._id = id
+    }
+
+    OR...
+
+    let obj
+
+    if (id) {
+        obj = {_id: id}
+    }
+    
+    */
+
+    const products = await ProductsModel.find(obj)
 
     res.send(products)
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const ProductsController = require('../controllers/productsController')
 
-router.get('/products', ProductsController.get)
+router.get('/products/:id?', ProductsController.get) // here ID parameter is optional ("?")
 //router.post('/products', ProductsController.post)
 //router.put('/products/:id', ProductsController.put)
 //router.delete('/products/:id', ProductsController.delete)

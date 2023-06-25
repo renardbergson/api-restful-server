c13453e72525dace4e2fc55afda137ced8b0e115
Renard Bergson <renardrock@gmail.com>
2023-06-25 12:45:47 -0300

    adicionando middleware de CORS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors')
+const app = express()
 
 // importing routes directory and file
 const routes = require('./routes/routes')
@@ -7,9 +9,38 @@ const routes = require('./routes/routes')
 const database = require('./database/database')
 database.connect()
 
-const app = express()
-app.use(express.json()) 
+// enabling and configuring CORS - allows other domains to consume our API (or not)
+// app.use(cors()) - Enable All CORS Requests
+const allowedOrigins = [
+    'http://127.0.0.1:5500',
+    'http://example.com.br'
+    // remind to to a hard refresh on browser to verify! (shift + refresh)
+]
+
+app.use(cors({
+    origin: function (origin, callback) {
+        let allowed = true
+
+        if (!origin) allowed = true
+        // because mobile app doesn't have an origin
+
+        if (!allowedOrigins.includes(origin)) allowed = false
+        // if the origin is not included in our list
+
+        callback(null, allowed)
+        // the first parameter is a message we want to return to who made the request
+        // the second one is if the response is allowed or not, true or false
+    }
+
+    /*
+        app.use(cors({
+            origin: allowedOrigins
+        }))
+    */
+}))
+
 // setting server to receive data on json format, we're not receiving data from form anymore
+app.use(express.json()) 
 
 // setting routes
 app.use('/api', routes)

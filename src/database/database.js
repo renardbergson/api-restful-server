07dc3205890c110f613c652d79076a1c44888a1d
Renard Bergson <renardrock@gmail.com>
2023-07-04 19:02:54 -0300

    configurando database para usar banco de dados remoto

--- src/database/database.js
+++ src/database/database.js
@@ -1,15 +1,25 @@
 // requiring mongoose to connect to database
 const mongoose = require('mongoose')
 
-// inserting methods inside a function to export
 function connect () {
-    //mongoose.set('useNewUrlParser', true)
-    //mongoose.set('useUnifiedTopology', true)
-
-    mongoose.connect('mongodb://localhost:27017/projeto-api-restful')
+    // remote conection with Mongodb - ATLAS
+    const username = 'admin'
+    const password = 'admin'
+    const database = 'api-restful'
+    
+    mongoose.connect(`mongodb+srv://${username}:${password}@api-restful.ceducaz.mongodb.net/${database}?retryWrites=true&w=majority`)
+    
     const db = mongoose.connection
+    
     db.once('open', () => console.log('application connected to database!'))
     db.on('error', () => console.error.bind(console, 'some error has ocurred!'))
+
+    /* LOCAL CONECTION
+        mongoose.connect('ex: mongodb://localhost:27017/projeto-api-restful')
+        const db = mongoose.connection
+        db.once('open', () => console.log('application connected to database!'))
+        db.on('error', () => console.error.bind(console, 'some error has ocurred!'))
+    */
 }
 
 module.exports = {connect}

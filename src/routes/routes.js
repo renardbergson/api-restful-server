c56477257dd5ad5573035e5a01183d92fa8e8331
Renard Bergson <renardrock@gmail.com>
2023-07-04 21:15:09 -0300

    ...

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 
 const ProductsController = require('../controllers/productsController')
 
+router.get('/', (req, res) => {res.send('hello world!')})
 router.get('/products/:id?', ProductsController.get) // here ID parameter is optional ("?")
 router.post('/products', ProductsController.post)
 router.put('/products/:id', ProductsController.put)
